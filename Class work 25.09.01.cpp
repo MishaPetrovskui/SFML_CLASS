99b0ac519f273fcdeea65637aad4577b6194239f
MishaPetrovskui <154260100+MishaPetrovskui@users.noreply.github.com>
2025-09-01 20:36:43 +0300

    Create Class work 25.09.01.cpp

---
+++ Class work 25.09.01.cpp
@@ -0,0 +1,52 @@
+#include <SFML/Graphics.hpp>;
+
+using namespace sf;
+using namespace std;
+
+Color MAIN_COLOR(57, 62, 173, 0);
+float speed = 0.1f;
+int main()
+{
+	RenderWindow window(VideoMode({200,200}), "SFML");
+
+	Vertex figure[] =
+	{
+		{{100,100}, Color::Red, {0,0}},
+		{{120,120}, Color::Red, {0,0}},
+		{{80,120}, Color::Red, {0,0}},
+		{{100,100}, Color::Red, {0,0}}
+	};
+
+	CircleShape Circle(50);
+	Circle.setPosition({ 0,0 });
+	Circle.setFillColor(Color::Red);
+
+	while (window.isOpen())
+	{
+		while (const optional event = window.pollEvent())
+		{
+			if (event->is<Event::Closed>())
+				window.close();
+		}
+		window.clear(Color::White);
+		//window.draw(figure, 4, PrimitiveType::Triangles);
+		if (Circle.getPosition().x <= 100 && Circle.getPosition().y == 0)
+		{
+			Circle.setPosition(Circle.getPosition() + Vector2f(speed, 0));
+			/*if (Circle.getPosition().x == 100)
+			{
+				Circle.setPosition(Circle.getPosition().x, Circle.getPosition().y + 1)
+			}*/
+		}
+		else if (Circle.getPosition().x >= 100)
+		{
+			Circle.setPosition(Circle.getPosition() - Vector2f(0, speed));
+		}
+		
+		window.draw(Circle);
+		window.display();
+	}
+
+
+	return 0;
+}
