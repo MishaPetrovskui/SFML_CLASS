e4ba88cb1488b0b6c17787b9a83b69c9082cdfd8
MishaPetrovskui <154260100+MishaPetrovskui@users.noreply.github.com>
2025-09-08 19:15:07 +0300

    Create Class work 25.09.08.cpp

---
+++ Class work 25.09.08.cpp
@@ -0,0 +1,87 @@
+#include <SFML/Graphics.hpp>;
+#include <iostream>;
+#include <Math.h>;
+
+using namespace sf;
+using namespace std;
+
+Color MAIN_COLOR(57, 62, 173, 0);
+int main()
+{
+	RenderWindow window(VideoMode({700,700}), "SFML");
+
+	CircleShape shape(50.f);
+	shape.setFillColor(Color::Green);
+	shape.setOrigin({ shape.getRadius(), shape.getRadius() });
+	float shape_speed = 0.5f;
+	Vector2f pos_of_mouse;
+	Vector2f pos_of_mouse1;
+	while (window.isOpen())
+	{
+		while (const optional event = window.pollEvent())
+		{
+			if (event->is<Event::Closed>())
+				window.close();
+			if (event->is<Event::KeyPressed>() && event->getIf<Event::KeyPressed>()->code == Keyboard::Key::Escape)
+				window.close();
+			if (event->is<Event::KeyPressed>() && event->getIf<Event::KeyPressed>()->code == Keyboard::Key::Numpad1)
+				MAIN_COLOR = Color::Blue;
+			if (event->is<Event::KeyPressed>() && event->getIf<Event::KeyPressed>()->code == Keyboard::Key::Numpad2)
+				MAIN_COLOR = Color::Red;
+			if (event->is<Event::KeyPressed>() && event->getIf<Event::KeyPressed>()->code == Keyboard::Key::Numpad3)
+				MAIN_COLOR = Color::Black;
+
+			/*if (event->is<Event::KeyPressed>() && event->getIf<Event::KeyPressed>()->code == Keyboard::Key::D)
+				shape.setPosition(shape.getPosition() + Vector2f(10, 0));
+			if (event->is<Event::KeyPressed>() && event->getIf<Event::KeyPressed>()->code == Keyboard::Key::A)
+				shape.setPosition(shape.getPosition() + Vector2f(-10, 0));
+			if (event->is<Event::KeyPressed>() && event->getIf<Event::KeyPressed>()->code == Keyboard::Key::W)
+				shape.setPosition(shape.getPosition() + Vector2f(0, -10));
+			if (event->is<Event::KeyPressed>() && event->getIf<Event::KeyPressed>()->code == Keyboard::Key::S)
+				shape.setPosition(shape.getPosition() + Vector2f(0, 10));*/
+
+			if (event->is<Event::MouseButtonPressed>() && event->getIf<Event::MouseButtonPressed>()->button == Mouse::Button::Left)
+			{
+				//shape.setPosition((Vector2f)Mouse::getPosition(window));
+				pos_of_mouse = (Vector2f)Mouse::getPosition(window);
+			}
+			if (event->is<Event::MouseButtonPressed>() && event->getIf<Event::MouseButtonPressed>()->button == Mouse::Button::Right)
+			{
+				//shape.setPosition((Vector2f)Mouse::getPosition(window));
+				pos_of_mouse1 = (Vector2f)Mouse::getPosition(window);
+			}
+			if (event->is<Event::MouseButtonPressed>() && event->getIf<Event::MouseButtonPressed>()->button == Mouse::Button::Middle)
+			{
+				shape.setRadius(shape.getRadius() + 1.f);
+			}
+		}
+		
+		if (Keyboard::isKeyPressed(Keyboard::Key::W))
+			shape.move({ 0, -0.5 });
+		if (Keyboard::isKeyPressed(Keyboard::Key::S))
+			shape.move({ 0, 0.5 });
+		if (Keyboard::isKeyPressed(Keyboard::Key::A))
+			shape.move({ -0.5, 0 });
+		if (Keyboard::isKeyPressed(Keyboard::Key::D))
+			shape.move({ 0.5, 0 });
+
+		if (shape.getPosition().x < pos_of_mouse.x)
+			shape.move({ shape_speed, 0 });
+		if (shape.getPosition().x > pos_of_mouse.x)
+			shape.move({ -shape_speed, 0 });
+		if (shape.getPosition().y < pos_of_mouse.y)
+			shape.move({ 0, shape_speed });
+		if (shape.getPosition().y > pos_of_mouse.y)
+			shape.move({ 0, -shape_speed });
+
+		if (pos_of_mouse1.x - shape.getPosition().x > 1 && pos_of_mouse1.y - shape.getPosition().y > 1)
+			shape.move({ shape_speed, shape_speed });
+
+		window.clear(MAIN_COLOR);
+		window.draw(shape);
+		window.display();
+	}
+
+
+	return 0;
+}
